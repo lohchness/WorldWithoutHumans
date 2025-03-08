9835a4a865a666d894d8c80fb291d75df370f82d
Lok/Tom Chow <lohchness@gmail.com>
2025-03-08 15:39:08 +1100

    Warning is a bomb that explodes on a timer

---
+++ scripts/warning.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var target: CharacterBody2D
+var target_acquired = false
+
+var damage = 10
+
+func _ready() -> void:
+	target = get_tree().get_first_node_in_group("Player")
+
+func _on_explode_timer_timeout() -> void:
+	
+	if target_acquired:
+		target.damage(damage)
+	queue_free()
+
+func _on_explode_radius_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		target_acquired = true
+
+func _on_explode_radius_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		target_acquired = false
