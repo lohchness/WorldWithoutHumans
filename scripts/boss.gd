116a4250e63077efb0dae4f25e933b4925ece3cd
Lok/Tom Chow <lohchness@gmail.com>
2025-03-08 18:03:25 +1100

    Boss Phase 1 complete

--- scripts/boss.gd
+++ scripts/boss.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const PHASE_1_SPEED: int = 120
+const PHASE_1_SPEED: int = 70
 const PHASE_1_ACCEL: int = 10
 
 var speed: float
@@ -37,9 +37,32 @@ func _on_player_detected_state_entered() -> void:
 
 ## PHASE 1
 
+# Boss starts with artillery on cooldown
+var p1_attack_cooldown: float = 2.0
+var p1_attack_timer: float = 0.0
+var big_warning: PackedScene = preload("res://scenes/big_warning.tscn")
+
 func _on_phase_1_state_entered() -> void:
 	face_sprite.texture = happy_face
 
-
 func _on_walk_state_physics_processing(delta: float) -> void:
-	pass # Replace with function body.
+	var dir = (target.global_position - global_position).normalized()
+	velocity = dir * speed
+	move_and_slide()
+	
+	p1_attack_timer += delta
+	if p1_attack_timer >= p1_attack_cooldown:
+		phases_sc.send_event("on_attack")
+
+## Stationary only serves to "prime" the attack
+func _on_stationary_state_physics_processing(delta: float) -> void:
+	velocity = Vector2.ZERO
+	move_and_slide()
+
+func _on_stationary_state_exited() -> void:
+	# Artillery attack after "priming" in Stationary state
+	p1_attack_timer = 0
+	
+	var s: Node2D = big_warning.instantiate()
+	s.global_position = target.global_position + target.velocity / 3
+	get_tree().root.add_child(s)
