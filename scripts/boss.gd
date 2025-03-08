a4d29c884525febde67880cd095881f8df2ec8d5
Lok/Tom Chow <lohchness@gmail.com>
2025-03-08 16:56:31 +1100

    initial boss code

--- scripts/boss.gd
+++ scripts/boss.gd
@@ -1,1 +1,15 @@
 extends CharacterBody2D
+
+const PHASE_1_SPEED: int = 120
+const PHASE_1_ACCEL: int = 10
+
+
+var speed: float
+var accel: float
+
+var target: CharacterBody2D
+
+func _ready() -> void:
+	target = get_tree().get_first_node_in_group("Player")
+	speed = PHASE_1_SPEED
+	accel = PHASE_1_ACCEL
