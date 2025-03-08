32ad1725b6ea94f78286b2f2d4fe85f2bcb9d552
Lok/Tom Chow <lohchness@gmail.com>
2025-03-08 14:37:29 +1100

    Refactor beam weapon to state chart

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,8 +8,12 @@ var health: float = 100.0
 
 var direction : Vector2
 
+var a1_attack: bool = false
+@onready var beam_weapon = $BeamWeapon
+
 signal gameover
 
+@onready var weapon_sc: StateChart = $WeaponStateChart
 
 func _physics_process(delta: float) -> void:
 	direction = Input.get_vector("left", "right", "up", "down")
@@ -20,10 +24,8 @@ func _physics_process(delta: float) -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("fire_laser"):
-		var areas = $Marker/Area2D.get_overlapping_areas()
-		print(len(areas))
-		for i in areas:
-			i.destroy()
+		weapon_sc.send_event("on_a1_attack")
+		
 
 func damage(amount: float) -> void:
 	health -= amount
@@ -31,3 +33,21 @@ func damage(amount: float) -> void:
 	
 	if health <= 0:
 		gameover.emit()
+
+## WEAPON STATES
+
+func _on_a_1_attacking_state_entered() -> void:
+	# Enable beam functionality
+	a1_attack = true
+	beam_weapon.visible = true
+	
+
+func _on_a_1_attacking_state_exited() -> void:
+	# Disable beam functionality
+	a1_attack = false
+	beam_weapon.visible = false
+
+func _on_a_1_attacking_state_physics_processing(delta: float) -> void:
+	var areas = $Marker/Area2D.get_overlapping_areas()
+	for i in areas:
+		i.destroy()
