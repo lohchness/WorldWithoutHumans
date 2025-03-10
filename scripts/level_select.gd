7c95c27310836c02f955e103daa25b970c5fe2b7
Lok/Tom Chow <lohchness@gmail.com>
2025-03-10 20:17:22 +1100

    Add level 1 selectoin

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+func _on_level_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
