f61dc0a62e5fa13bc70f95a3621b3152f8b7f3a9
Lok/Tom Chow <lohchness@gmail.com>
2025-03-10 19:40:30 +1100

    Update main_menu.gd

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/leveltest.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
