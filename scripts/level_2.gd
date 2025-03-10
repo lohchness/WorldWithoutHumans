71c20341d8f660da1a2aacb7bd2ae76313c7c824
Lok/Tom Chow <lohchness@gmail.com>
2025-03-10 22:34:47 +1100

    Fighter jets spawn after crossing area2d

---
+++ scripts/level_2.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var player_trigger = false
+
+var jet_spawners: Array
+@onready var jet = preload("res://scenes/fighter_jets.tscn")
+
+func _ready() -> void:
+	jet_spawners = $FighterJetSpawner.get_children()
+
+func _on_jetfighter_trigger_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		if not player_trigger:
+			player_trigger = true
+			$"Jetfighter Timer".start()
+
+
+func _on_jetfighter_timer_timeout() -> void:
+	for i in range(2):
+		var s = jet.instantiate()
+		s.global_transform = jet_spawners.pick_random().global_transform
+		get_tree().root.add_child(s)
