236b94111ff4074a7c4cd4268164387ab390b03f
Lok/Tom Chow <lohchness@gmail.com>
2025-03-10 23:06:35 +1100

    level 3 select

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -11,3 +11,7 @@ func _on_level_1_pressed() -> void:
 
 func _on_level_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+
+
+func _on_level_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_3.tscn")
