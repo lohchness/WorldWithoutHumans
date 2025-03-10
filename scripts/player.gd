8d9cd85b444ff5c01c7afa0a434cfbdf1a13b996
Lok/Tom Chow <lohchness@gmail.com>
2025-03-10 12:27:38 +1100

    Laser cooldown shows on ability hotbar

--- scripts/player.gd
+++ scripts/player.gd
@@ -16,14 +16,33 @@ signal gameover
 
 @onready var marker = $Marker
 
+@onready var laser_cooldown_timer: Timer = $Abilities/Timers/Ability1Cooldown
+@onready var magnet_cooldown_timer: Timer = $Abilities/Timers/Ability2Cooldown
+
+@onready var laser_timer_background = $Abilities/Icons/Cooldown/Ability1TimerBackground
+@onready var laser_timer_text = $Abilities/Icons/Cooldown/Ability1TimerText
+
+@onready var laser_finish_attack_transition = $"WeaponStateChart/Weapons/A1/A1 Attacking/On Finish Attack"
+@onready var laser_cooldown_finish_transition = $"WeaponStateChart/Weapons/A1/A1 Cooldown/On Cooldown Finish"
+
+var laser_on_cooldown = false
+
 func _ready() -> void:
 	magnet_area.monitoring = false
+	
+	laser_cooldown_timer.wait_time = int(laser_finish_attack_transition.delay_in_seconds) + int(laser_cooldown_finish_transition.delay_in_seconds)
 
 func _physics_process(delta: float) -> void:
 	direction = Input.get_vector("left", "right", "up", "down")
 	velocity.x = move_toward(velocity.x, speed * direction.x, accel)
 	velocity.y = move_toward(velocity.y, speed * direction.y, accel)
 	move_and_slide()
+	
+	## Update ability hotbar
+	if laser_on_cooldown:
+		laser_timer_background.visible = true
+		laser_timer_text.visible = true
+		laser_timer_text.text = str(ceil(laser_cooldown_timer.time_left))
 
 
 func _input(event: InputEvent) -> void:
@@ -45,6 +64,8 @@ func damage(amount: float) -> void:
 func _on_a_1_attacking_state_entered() -> void:
 	# Enable beam functionality
 	beam_weapon.visible = true
+	laser_cooldown_timer.start()
+	laser_on_cooldown = true
 
 func _on_a_1_attacking_state_exited() -> void:
 	# Disable beam functionality
@@ -72,3 +93,8 @@ func _on_magnet_area_body_entered(body: Node2D) -> void:
 
 func picked_up_corpse():
 	print("hello")
+
+func _on_ability_1_cooldown_timeout() -> void:
+	laser_on_cooldown = false
+	laser_timer_background.visible = false
+	laser_timer_text.visible = false
