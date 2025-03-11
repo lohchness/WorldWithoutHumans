d4dc60ea3e9f21d3caa29c15005b1533fa3c3e93
Lok/Tom Chow <lohchness@gmail.com>
2025-03-11 13:27:39 +1100

    move hp bar to camera on boss detect

--- scripts/level_3.gd
+++ scripts/level_3.gd
@@ -2,12 +2,28 @@ extends Node2D
 
 var infantry_spawners: Array
 @onready var infantry = preload("res://scenes/infantry.tscn")
-@onready var boss_hp_bar: BossHPBar = $"Boss HP Bar"
+@onready var boss_hp_bar: BossHPBar = $"Player/Camera2D/Boss HP Bar"
 
 func _ready() -> void:
 	infantry_spawners = $"Infantry Spawners".get_children()
-	$Boss.connect("health_changed", $"Boss HP Bar".update_boss_health)
+	$Boss.connect("health_changed", boss_hp_bar.update_boss_health)
+	$Boss.connect("player_detected", move_hp_bar)
+	
+	origpos = Vector2(boss_hp_bar.position)
 
+var origpos: Vector2
+var targpos = Vector2(0, -290)
+var healthon = false
+
+func _physics_process(delta: float) -> void:
+	if healthon:
+		boss_hp_bar.position = lerp(boss_hp_bar.position, targpos, 5 * delta)
+
+func move_hp_bar():
+	#targpos = origpos + Vector2(0, 63)
+	#print("New position: " + str(origpos + targpos))
+	print("here")
+	healthon = true
 
 func _on_infarty_timer_timeout() -> void:
 	for i in range(2):
