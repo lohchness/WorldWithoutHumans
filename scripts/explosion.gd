14de32f9227daaf4365727b9c30b1375cafa5c42
Lok/Tom Chow <lohchness@gmail.com>
2025-03-11 12:10:22 +1100

    choose random explosion on death

--- scripts/explosion.gd
+++ scripts/explosion.gd
@@ -1,5 +1,8 @@
 extends AnimatedSprite2D
-@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+@export var players: Array[AudioStreamPlayer2D] = []
+@onready var chosen_player: AudioStreamPlayer2D = null
+
 
 func _ready() -> void:
 	var num_anims = sprite_frames.get_animation_names().size()
@@ -7,11 +10,13 @@ func _ready() -> void:
 	animation = sprite_frames.get_animation_names()[rand]
 	play()
 	
-	if audio_stream_player_2d:
-		audio_stream_player_2d.play()
+	if len(players) > 0:
+		chosen_player = players.pick_random()
+		chosen_player.connect("finished", _on_animation_finished)
+		chosen_player.play()
 
 func _on_animation_finished() -> void:
-	if audio_stream_player_2d:
+	if chosen_player:
 		visible = false
 	else:
 		queue_free()
